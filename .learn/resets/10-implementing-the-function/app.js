ad29721d21ec47c313285b57e2df80b0fef2d6f3
Jide Muritala <115728688+Jide-Muritala@users.noreply.github.com>
2023-06-01 19:31:42 +0000

    testing with jest

---
+++ .learn/resets/10-implementing-the-function/app.js
@@ -0,0 +1,16 @@
+// this is my function that sums two numbers
+const sum = (a,b) => {
+    return a + b
+}
+
+// just a console log for ourselves.
+console.log(sum(7,3))
+
+// export the function to be used on other files 
+// (similar to the keyword `export` when using webpack)
+module.exports = { sum };
+
+// this is my function that converts dollar to yen
+const fromDollarToYen = (dollarAmount) => {
+    return a + b
+}
